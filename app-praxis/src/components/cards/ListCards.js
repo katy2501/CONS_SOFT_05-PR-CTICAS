d6d92e306518d1fe4f0297477b14410ba44600cc
Virgini2501 <86138958+katy2501@users.noreply.github.com>
2021-06-23 11:00:20 -0500

    Add files via upload

---
+++ app-praxis/src/components/cards/ListCards.js
@@ -0,0 +1,12 @@
+import Cards from "./Cards"
+
+const ListCards = () => {
+    return (
+        <div>
+            <h1>ListCards</h1>
+            <Cards/>
+        </div>
+    )
+}
+
+export default ListCards
