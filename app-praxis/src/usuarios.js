bff53ec0b5ca73f1859ba8f03b3f3260cd2a1b41
Virgini2501 <86138958+katy2501@users.noreply.github.com>
2021-07-12 22:10:41 -0500

    Add files via upload

---
+++ app-praxis/src/usuarios.js
@@ -0,0 +1,89 @@
+const users = [
+    {
+      "name": {
+        "title": "Mr",
+        "first": "Gustavo",
+        "last": "Diego"
+      },
+      "login": {
+        "uuid": "7b6e8b00-58e6-457e-9800-e9b77cb4a39b",
+        "username": "angrybird924",
+        "password": "lemmein",
+      },
+      "picture": {
+        "large": "https://lh3.googleusercontent.com/TL67VvFjqq5YqgN202Oi7LKyyV2UPonAdpi2FcVMfvJQaClLAYGJOQXWGiOmzc27hOvnkT4eW9yA4ucURCP48wsHF-sO491ZZSO5_GGwWgjTh30KhYsEo85XHGotraae8-DExHfm5fTlBI4ZsZBD3v62-pM1XcE4kzBezLBHZNiILaBvI_8lR1WfBpT0q5qu0YVN7RBQucJbgQNMBgl2UG35gSw0iNQjRG-kNycOBmijDuqfRheCACrByfKecLNIJdVZT2cu6GYb4BELZ0oXHyzY7QtsrwX6oyVymTp4N3a4GnzedQE9ttwqY1cpEq-AazYB1yURC_tRXVc8xn95KdjeLPziii4mqv1PtsxWrbKp052dHTQpzZPwGZIoa2wIAHbd3tLLrkIGVUcKeRhQGK2iTnP640uU0zt2w98vtM_f7m20xrAhVxnBGDYBgwtGDO85-smaV6KCr6i1sr_SrjJnx9o83RoGG17ufq18MBcxrsoirWPWD6PlMQsWerKDkujf-HDXneVNl4uGxJb2ir-Qkdn_FOKCQKiBsvaf-y9NpJX5N7rhaOMD_qsHt39O9kz_2ldgbo3rPC_JIsW6dx9yRDXQlD4fKqkgFV9b3X_bdWSTCappCcmPIxii1ejL2xFPwblJaJNpUKWHdVzDZHh-4MMTzMc5uq_iFUgDeJy0i3f0Idw-ERGvtPXzufa5NEO-d4qLHNgShbO159xFfIQ=w862-h1149-no?authuser=0",
+        "medium": "https://randomuser.me/api/portraits/med/men/46.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/46.jpg"
+      },
+    
+    },
+    {
+      "name": {
+        "title": "Mr",
+        "first": "Gaute",
+        "last": "Walter"
+      },
+      "login": {
+        "uuid": "16f93250-8a2e-4275-845d-4b62c7264013",
+        "username": "silverkoala959",
+        "password": "samiam",
+      },
+      "picture": {
+        "large": "https://lh3.googleusercontent.com/XooLCS0s7964ps7vlmFVh5VYA9PplNfytMeZWGhqE2c-eRRg0SOQ2PDa8JUaVrLMukxBu6OtBJTopZy0d_m3XlxtWjvdeNA72nKbcyyT74MJ38CKTS0SX-aHWC3uD9hsjO9vbnU5WY84D9sxLQsuPRfceUaHH1NBzfRRMjJlOcxE13NGEIy-J2BaB8BzuVoNHj7-oYpgBuH3VcLHEiZOPOtf0gQLR3QzTpBgGneYAwu1E7Bma_ih3BsFaSa3rCtFurAaVmeuOOtBEIuRqP6ERYR4gV_KLO26o0DXB68d79oaz7Rk_UO5qiLQVrCdqglkcLA-MtxN1PCHPGYgEh_8HjmGPWfRQcTqhQZIc6KExtUi-vj5nli-PtgpaygJYuuBru3WX44-uvvBuc4SQnMeGCQ-oIOJtk2S9P_uoICHHK9EigL51YKkxm_VoA0YxlNnysyYMLh4oW_eD0h2hMltLxqYwXCGM30FRyLCDWsyKBD8-9z51Hi6-uvMuFnIuoT7ZJsqPkFee3vu0E8GuOapgLBQFgQFLd0IeNy3nhfUCN67J0PyJxpf6ipLwh0BrjWcETdJ1Npl-RNlg-dJcwEgL6d4v-u5TY-2qVh-GddvnK6Xi8yeaht2m9RsuAXI_xdThvH6Tx1raFNnpGZ8gSrf09QfFkF2tulu71BL7l3XXpnSaCymcmkMrZYfmtwMolkPYV9QJgYnbDS7R9LxxicGH5k=w216-h288-no?authuser=0",
+        "medium": "https://randomuser.me/api/portraits/med/men/20.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/20.jpg"
+      },
+    },
+    {
+      "name": {
+        "title": "Mr",
+        "first": "Oliwier",
+        "last": "Valeria"
+      },
+      "login": {
+        "uuid": "cc7fee39-93cb-432b-b4c2-2b8788c2bd38",
+        "username": "whitemouse247",
+        "password": "banana",
+      },
+      "picture": {
+        "large": "https://lh3.googleusercontent.com/PAiWnH6GfypXdzxSHzPc017e0LUyzuJPxCi7EeypssvF-ny1vxY7Xf0-OKpjq-E7DrNpmHdtBGS9-5Qk4FBwnSrAKa3-dlowI25fnpJy_YUZMSHC6_pbbpH0kK0MJX2bahMnCto7zsgL3Zn_0kgy3VojtJdJGKxYdOKSe_1QyAYul2vd6P7AkLIqP3ypTqQQrExz5_wm0U2L1wTnQcr_kuYMaolIcG7B_NXtW43PgkCvBBuYCC1z_OgVphq4y2gbecchJJFkLF1VpAHWNBug53YJvX2_zXzhKLgnreYc7eIP9J7YR-LuNFhzVAjC5hKxbquFXxY-RxI_GEMU4_vXXuExEf_ugfnSeysIKwr4XO4ckCvZomReUaCUdTkR_wr1mdVhW-Ga0-2J2HeH4qAzsPFjkbMhJILrJ-IFvn2xGFmgEcAVjsH5Q3ZNepXf5RgpqVf0oZ7JWs8BPhZVUjKJAWIvTC-_8Ja2VJ1UQgpKgWTcw1U4ovhKotZOiYDKZ2WFiDvskSAVCTuhKSczdxzMyoWwT_vy6YzXoJEfSwazmHZsWJ2evktWC0K47l7r6ECIvtXV34N1oL0iTBbw72VHeovIV2lIkinboI5cDfWm_7Ps9ggLK6uEihwa9a08kMAcEcEBHj48pU7Hkd8kKsOVKeqs_NBq7U-GD1fApd6H23V7RpGhgNp22Ljn2ip5oHcTrq029KjUEqXyA8W7jAylY00=w215-h288-no?authuser=0",
+        "medium": "https://randomuser.me/api/portraits/med/men/41.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/41.jpg"
+      },
+    },
+    {
+      "name": {
+        "title": "Ms",
+        "first": "Beatrice",
+        "last": "Melina"
+      },
+      "login": {
+        "uuid": "a1c87fe7-291f-4a24-8690-a2bef6f9cd86",
+        "username": "brownpanda733",
+        "password": "daddy",
+      },
+      "picture": {
+        "large": "https://lh3.googleusercontent.com/j_9r0UN8Kr-aUEZ1-nAXrligA0-hFcbZSXv83nOm8LrRgQcLsjDLkDI46-D6jswSbo6L1byB6U0VEnaSOyH6Na2ECWJzHfaruPiuvOtLUH2uar4MduAXOyV7jOOicuTD4n775vMfRfnqEs9ZCqdYmB5NGjSzXpasS6_xJxb82CYvMy_80Fp5a_NUPhjf-Vwyq232XlNaK8BVu6oKbM54UMwTXyldFIzW-88Prep_vTqfhuKw66DVyUgseebegDAAvtF5DhLVtrTphgSr0Nc06wA_-1VFbRNzzS1JZdhYSDKdJ-ZViE-6WGrn2ieWJZ4t-pVEkcbVcS3vjnqna8zg3BXKvLfI1d6VohN_hG_7czCtK8UXQ_TH_ykoiCHT7bwCzQktiXmIDWztvXOGRcivDCGhd2ZDYSoxOhMRFIpkTnmKbnfWGImw2GIH5MjQ96Sv5NWzb-rXoU4ofqnWALCLBWTUl1CVSeL-GIzYXxpAUEViI_RdUBYCJiOqHhPOkNmxJS7k_QJ_f2aRn0xEcZ-_yvs-FZBUYgBttnmYdM4rO3mhHdu8yspTK_legFnGkBZJsNgi4ffu9fKBFxrn79gqkioC92ZIIzFMyrDYJcPJzCyGoIp9WP_4KBnHcRz2XCI7bcvmTDTCoX8zwhPL7Cwxe4VQ8NkiRQtvGilAl5W5A955OepW0U5ECww_Gcb7sXv0FnPTryb1brXydlKRfmiZK44=w672-h739-no?authuser=0",
+        "medium": "https://randomuser.me/api/portraits/med/women/0.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/women/0.jpg"
+      },
+    },
+    {
+      "name": {
+        "title": "Mr",
+        "first": "Fred",
+        "last": "Anonimo"
+      },
+      "login": {
+        "uuid": "982838b4-3e24-4332-9a36-7343bb7b00c5",
+        "username": "yellowsnake109",
+        "password": "nick",
+      },
+      "picture": {
+        "large": "https://lh3.googleusercontent.com/FvkMw6z8Wp4Fu4EWXFolSUD97cbnuQJ66ngEsnqaSfKMAzMEr9V7DXPX-LUUfzR5IwfWNbY-FTBEYm8pWXhYxbRFJxk0hiIi-HdmYT314H_ajDfLYhKG8LzG6LRsY0s9s3VSx0_y-HZgE2NGC0wjx6cyztRAPEJbFGTbQ8vR3LQTLawdmxmp6tiY79EklbxFWQcj0W8ppEtFEf7vkAymlQTRffTnzFbBFXWHwH1q2lvGxN01QFdhET0p_IUnpDw7MekXfY2q4vml3mO14W9v-BqilPcEL1wmMr6qQ1JvtAUNmdwvZXJlUfFGxXbHN9aVOu_G3X0epBswJbcBp88MyI2k7i9qGKxg_zxje_QnUQk1LpfXUqjML9d-__98Uk0UBiwcCnnLq8jwCFjP_RzL78d6zt3upLwNc8KfwKXo1rgNMZIzs8mz6VuIGdnqit4n4BfPOnXPL39vV70O4hihHpIcjTCLdEkWE_dFQPt_5B-lu4taDjQpeSqNxX_5-Vgfs_x0pGqb1zo-l48CjnsOs5w2JgKWfxh3hnSevgAjfyGi4NrOZzJL-Rr9GTzMgNc5OgTXYzI3vWj9fd9CnmBIdqwwK8j5u44pBesZmh6myStT9Lo64gCes9jtgn02HRwxAGw-vHNjO0Q3zSVwfUbZ0ObZCKT7K5ZuS4WnDbI1exdYAVzxyWE-wxM72OXGHdXDf09bby-D0DsugV0B8GbCPD0=w316-h351-no?authuser=0",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/41.jpg"
+      },
+    }
+  ]
+
+  export default users
