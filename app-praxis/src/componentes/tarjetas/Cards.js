491ea3fa57991acd4bece354ac729b4f5ac11d5a
Virgini2501 <86138958+katy2501@users.noreply.github.com>
2021-07-12 21:16:21 -0500

    Update and rename app-praxis/src/components/cards/Cards.js to app-praxis/src/componentes/tarjetas/Cards.js

---
+++ app-praxis/src/componentes/tarjetas/Cards.js
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+// import foto from'../../componets/image/PicsArt_06-06-08.10.12.jpg'
+const Cards = ({user}) => {
+    const {name,login,picture} = user
+    const [color, setcolor] = useState(true)
+    return (
+        <div className="card">
+            <img src={picture.large} className="card-img-top" alt={login.userName}/>
+            <div className="card-body">
+                <h5 className="card-title">{name.last}</h5>
+                <button className={color ? "btn btn-primary" : "btn btn-light"} onClick= {() => setcolor(!color)}>Seguir</button>
+            </div>
+        </div>
+
+    )
+}
+export default Cards
