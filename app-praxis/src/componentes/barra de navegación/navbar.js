c71cce5e025f6fb3234c4591a2f7eba88aa8c80c
Virgini2501 <86138958+katy2501@users.noreply.github.com>
2021-07-12 21:17:47 -0500

    Update and rename app-praxis/src/components/navbar/navbar.js to app-praxis/src/componentes/barra de navegación/navbar.js

---
+++ app-praxis/src/componentes/barra de navegación/navbar.js
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+
+const Navbar = () => { 
+    const[user, setUser] = useState(null)
+    const login = () => {
+      setUser({id:1,nombre:'Lbarcoc2'})
+    }
+    const logaut = () => {
+      setUser(null)
+    }
+    return(
+      <nav className="Navbar Navbar-light bg-success bg-gradient">
+            <div className="container-fluid">
+              <span className="navbar-brand mb-0  fs-4 text-white">User:{user ? user.nombre : 'No Autentificado'}</span>
+              { user ? (<button type="button" class="btn btn-outline-light" onClick = {logaut}>Cerrar Seccion</button>) : (<button type="button" class="btn btn-outline-light" onClick = {login}>Iniciar Seccion</button>)}
+            </div>
+      </nav>
+    )
+}
+export default Navbar
