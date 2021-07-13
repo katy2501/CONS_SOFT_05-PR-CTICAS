cc27008432f07fae57ed0909aed81dc0583415f8
Virgini2501 <86138958+katy2501@users.noreply.github.com>
2021-07-12 21:12:32 -0500

    Update and rename app-praxis/src/components/cards/ListCards.js to app-praxis/src/componentes/tarjetas/ListCards.js

---
+++ app-praxis/src/componentes/tarjetas/ListCards.js
@@ -0,0 +1,35 @@
+import Cards from './Cards'
+//import celebridades from '../../helpers/usuarios'
+import React, { useEffect, useState} from 'react'
+
+const ListCards= () => {
+    const [celebridades, setcelebridades] = useState([])
+    const [error, setError] = useState(false)
+    useEffect(() => {
+        const etusuarios = async() => {
+            try{
+                 const res = await fetch("https://randomuser.me/api/?results=70")
+                 const data = res.json()
+                 setcelebridades(data.results)
+                 setError(false)
+             }catch(err) {
+            console.log(err)
+            setError(true)
+            }
+        }, []}
+    )
+    // console.log(celebridades[0].name.last)
+    return (
+        <div className="container">
+            <div className="row">
+            { celebridades.map(users => (
+                <div className="col-sm-6 col-nd-4 col-lg-3" key={users.login.uuid}>
+                    <Cards user={user}/>
+                </div>
+            ))
+            }
+            </div>
+        </div>
+    ) 
+}
+export default ListCards
