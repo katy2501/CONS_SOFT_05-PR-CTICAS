2e153debf276f232d19cd579b1e5aefcb0c711ce
Virginia Angulo 03 <86138958+katy2501@users.noreply.github.com>
2021-07-16 15:28:51 -0500

    Update ListCards.js

--- app-praxis/src/componentes/tarjetas/ListCards.js
+++ app-praxis/src/componentes/tarjetas/ListCards.js
@@ -16,20 +16,26 @@ const ListCards= () => {
                console.log(err)
                setError(true)
             }
-        }, []}
-    )
-    // console.log(celebridades[0].name.last)
+             }
+             getUsuario()
+         }, [])
+    if (error){
+        return <h1 class="alert alert-success" role="alert">
+                  Error al ingresar al api <a href="https://randomuser.me" class="alert-link">Verifique el link</a>
+               </h1>
+    }
     return (
         <div className="container">
-            <div className="row">
-            { celebridades.map(users => (
-                <div className="col-sm-6 col-nd-4 col-lg-3" key={users.login.uuid}>
-                    <Cards user={user}/>
-                </div>
-            ))
-            }
+         <div className="row">
+         { usuarios.map(user => (
+             <div className="col-md-4" key={user.login.uuid}>
+                  <Cards user={user}/>
             </div>
-        </div>
-    ) 
+         ))}
+                 
+             </div>
+         </div>
+
+    )
 }
 export default ListCards
